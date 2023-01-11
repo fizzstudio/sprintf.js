675629ce45b09e84c9fbc0c0c54b50cb96620868
jfnl88 <89284758+jfnl88@users.noreply.github.com>
2023-01-10 20:09:56 -0500

    Added TS type definitions file.

---
+++ src/main.d.ts
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {string} key
+ * @returns {string}
+ */
+export function sprintf(key: string, ...args: any[]): string;
+/**
+ *
+ * @param {string} fmt
+ * @param {Array} argv
+ * @returns {string}
+ */
+export function vsprintf(fmt: string, argv: any[]): string;
